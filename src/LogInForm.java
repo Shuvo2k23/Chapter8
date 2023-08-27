9acc3dc2c5358ccaa5a3cb7c4aea8dd945d455c6
shuvo2k23 <rshuvo036@gmail.com>
2023-08-27 21:37:49 +0600

    Problem -26, E8.26 LogInForm

--- src/LogInForm.java
+++ src/LogInForm.java
@@ -23,10 +23,7 @@ public class LogInForm {
         {
             enteredPassword = input;
         }
-        else
-        {
-            System.out.println("Something Went Wrong: input");
-        }
+
     }
 
 
